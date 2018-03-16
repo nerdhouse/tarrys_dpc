7351dacbcf95ad2be58089f2613f9f1b113037cd
Misha Wagner <mishajw@gmail.com>
2018-03-16 11:16:38 +0000

    Add skeleton code for creating a topology and spawning processes for each node

--- harrys.erl
+++ harrys.erl
@@ -1,5 +1,28 @@
 -module(harrys).
--export([main/0]).
+-compile(export_all).
 
-main() -> io:fwrite("Hello, world!\n").
+-record(node, {name, neighbours = []}).
+
+%% Create a process for every node in a network
+create_pids(Topology) ->
+  {InitialNodeName, Nodes} = Topology,
+  [spawn(harrys, work, [N]) || N <- Nodes].
+
+work(Node) ->
+  io:fwrite("~p: I am ~p~n", [self(), Node#node.name]),
+  ok.
+
+%% Get the topology of the network
+%% Returns a tuple of the name of the initial node, and a list of nodes
+get_topology() -> {
+    a,
+    [
+      #node{name = a, neighbours = [b, c]},
+      #node{name = b, neighbours = [a, c]},
+      #node{name = c, neighbours = [a, b]}]
+ }.
+
+main() ->
+  Tuple = get_topology(),
+  create_pids(Tuple).
 
