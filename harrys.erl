feca4af1eaf991313be91220cc3bfe087fab48b3
Misha Wagner <mishajw@gmail.com>
2018-03-16 13:15:51 +0000

    Add documentation and clean up code

--- harrys.erl
+++ harrys.erl
@@ -4,26 +4,31 @@
 -record(node, {name, neighbours = []}).
 
 %% Create a process for every node in a network
-create_pids(Topology) ->
-  {InitiatorName, Nodes} = Topology,
+start_network({InitiatorName, Nodes}) ->
+  %% Spawn a process for each node and store the PIDs
   NodePids = [{N#node.name, spawn(harrys, work, [N])} || N <- Nodes],
-  %% io:fwrite("NodePids: ~p~n", [NodePids]),
+  %% Let each node know what the PIDs of the other nodes are
   [ Pid ! NodePids || {_, Pid} <- NodePids],
+  %% Get the initiator's PID
   {_, InitiatorPid} = lists:keyfind(InitiatorName, 1, NodePids),
+  %% Send the initial token to the initial node
   InitiatorPid ! [{root, self()}].
 
 work(Node) ->
   receive
     NodePids ->
-      work(
-        Node,
-        zip_neighbours(NodePids, Node#node.neighbours),
-        {},
-        [])
+      %% Only get the PIDs of the neighbours
+      NeighbourPids = [
+        {NodeName, Pid} ||
+        {NodeName, Pid} <- NodePids,
+        lists:member(NodeName, Node#node.neighbours)],
+      %% Recieve the node PIDs, and then re-call work with default values
+      work(Node, NeighbourPids, {}, [])
   end.
 work(Node, NodePids, InitialParentPid, SentToPids) ->
   receive
     Token ->
+      %% If the `InitialParentPid` was not set, set to the last PID in `Token`
       {ParentName, ParentPid} = case InitialParentPid of
         {} -> lists:last(Token);
         _ -> InitialParentPid
@@ -32,42 +37,45 @@ work(Node, NodePids, InitialParentPid, SentToPids) ->
       %% io:fwrite("~p: Current token: ~p~n", [Node#node.name, Token]),
       %% io:fwrite("~p: Parent PID: ~p~n", [Node#node.name, ParentPid]),
 
+      %% Get all nodes we haven't sent anything to
       UnsentNodePids = [
         {NodeName, Pid} || {NodeName, Pid} <- NodePids,
                not lists:member({NodeName, Pid}, SentToPids),
                Pid /= ParentPid],
       %% io:fwrite("~p: Unsent PIDs: ~p~n", [Node#node.name, UnsentNodePids]),
 
+      %% Append our name and PID to `Token`
       NewToken = (Token ++ [{Node#node.name, self()}]),
 
       case UnsentNodePids of
+        %% If we have no more nodes to send to, send back to our parent
         [] -> ParentPid ! NewToken;
         _ ->
           %% Select random element from `UnsentNodePids`
           {NodeName, Pid} = lists:nth(
             rand:uniform(length(UnsentNodePids)), UnsentNodePids),
+          %% Send the token on to the randomly selected node
           Pid ! NewToken,
           work(
             Node,
             NodePids,
             {ParentName, ParentPid},
+            %% Add the node we just sent to to `SentToPids`
             (SentToPids ++ [{NodeName, Pid}]))
       end
   end.
 
-zip_neighbours(NodePids, Neighbours) ->
-  [{NodeName, Pid} ||
-   {NodeName, Pid} <- NodePids, lists:member(NodeName, Neighbours)].
-
-%% Get the topology of the network
+%% Get the topology of the network from `stdin`
 %% Returns a tuple of the name of the initial node, and a list of nodes
 get_topology() ->
+  %% Read the initial node name in from the first line
   [InitialNodeName | _] = string:tokens(io:get_line(""), " \n"),
   {InitialNodeName, get_topology([])}.
 get_topology(Nodes) ->
   case io:get_line("") of
     eof -> Nodes;
     NodeStr ->
+      %% Parse the line for a node
       [NodeName | Neighbours] = string:tokens(NodeStr, " \n"),
       get_topology(
         [#node{name = NodeName, neighbours = Neighbours} | Nodes])
@@ -75,7 +83,7 @@ get_topology(Nodes) ->
 
 main() ->
   Topology = get_topology(),
-  create_pids(Topology),
+  start_network(Topology),
   receive
     Token ->
       [_ | FinalToken] = [NodeName || {NodeName, _} <- Token],
